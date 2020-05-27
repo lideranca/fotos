f65bd6a3d0cdc7a758f3df88ac7ac03b269fd0a6
lojaslideranca <mauroecunha@gmail.com>
2020-05-27 11:43:15 -0300

    ''

--- pull.bat
+++
@@ -1,7 +0,0 @@
-cd "C:\Users\Vinicius\Documents\GitHub\fotos"
-sh
-exec ssh-agent bash
-ssh-add ~/.ssh/key
-ssh -T git@github.com                                                         
-git pull -a
-pause
